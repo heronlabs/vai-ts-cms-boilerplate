3f255f9be1710db87a26766ae817eb9818f3f3c7
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-03-29 11:26:05 -0300

    #deploy

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -3,6 +3,9 @@ export default ({env}) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID', ''),
+        secretAccessKey: env('AWS_ACCESS_SECRET', ''),
+        region: env('AWS_REGION', ''),
         params: {
           Bucket: env('AWS_BUCKET_NAME'),
         },
